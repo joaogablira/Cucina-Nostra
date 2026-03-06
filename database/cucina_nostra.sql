a00e919a3cda2cec5985bdd8dbcb38d31906f42b
João Gabriel Tavares de Lira <159500924+joaogablira@users.noreply.github.com>
2026-03-06 12:24:28 -0300

    Add files via upload

--- database/cucina_nostra.sql
+++ database/cucina_nostra.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 03/03/2026 às 21:35
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.0.30
+-- Tempo de geração: 06-Mar-2026 às 16:24
+-- Versão do servidor: 10.4.24-MariaDB
+-- versão do PHP: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,16 +24,16 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `categories`
+-- Estrutura da tabela `categories`
 --
 
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Despejando dados para a tabela `categories`
+-- Extraindo dados da tabela `categories`
 --
 
 INSERT INTO `categories` (`id`, `name`) VALUES
@@ -45,7 +45,7 @@ INSERT INTO `categories` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `recipes`
+-- Estrutura da tabela `recipes`
 --
 
 CREATE TABLE `recipes` (
@@ -58,12 +58,20 @@ CREATE TABLE `recipes` (
   `image_url` varchar(255) DEFAULT NULL,
   `status` enum('pending','approved') DEFAULT 'pending',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `recipes`
+--
+
+INSERT INTO `recipes` (`id`, `user_id`, `category_id`, `title`, `ingredients`, `preparation`, `image_url`, `status`, `created_at`) VALUES
+(1, 1, 1, 'Macarrão de Joãozinho', 'Macarrão de joaozinho\r\nMolho de tomate\r\noregano\r\nsal\r\npimenta\r\nfolha de manjericão', 'joga tudo na panela', 'assets/img/uploads/69aae6a639b60.jpg', 'pending', '2026-03-06 14:37:26'),
+(2, 2, 4, 'Sopa de Pedra', '300g de brita triturada\r\n500g de mármore carrara em cubos\r\n2L de água do rio Tietê\r\ncimento a gosto\r\nparalelepípedo moído para temperar', 'Refogue a brita triturada com sal e cebola. Em seguida adicione o mármore até ele ficar dourado. Jogue tudo numa vasillha grande e termine adicionando a água do rio, cimento e o paralelepípedo.', 'assets/img/uploads/69aaea6694f6b.png', 'pending', '2026-03-06 14:53:26');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `users`
+-- Estrutura da tabela `users`
 --
 
 CREATE TABLE `users` (
@@ -72,12 +80,20 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`) VALUES
+(1, 'Suzuki Cometi Seppuku', 'arthur_suzukas@gmail.com', '$2y$10$lCk.TQ8vJ420ovufT7ae7uMT8XMmaHBpYz9yQ6ZJDgY7blmxZAcfa', '2026-03-06 14:34:40'),
+(2, 'No-Lock', 'no.lock@gmail.com', '$2y$10$ce.sZz83GdDcpTjUr8trIOo8qnCkfKHaWK0hnmCTCdCVfO0yY5./e', '2026-03-06 14:44:08');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `votes`
+-- Estrutura da tabela `votes`
 --
 
 CREATE TABLE `votes` (
@@ -85,20 +101,29 @@ CREATE TABLE `votes` (
   `user_id` int(11) DEFAULT NULL,
   `recipe_id` int(11) DEFAULT NULL,
   `voted_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `votes`
+--
+
+INSERT INTO `votes` (`id`, `user_id`, `recipe_id`, `voted_at`) VALUES
+(1, 1, 1, '2026-03-06 14:37:38'),
+(3, 2, 1, '2026-03-06 14:44:35'),
+(6, 2, 2, '2026-03-06 14:53:36');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `categories`
+-- Índices para tabela `categories`
 --
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `recipes`
+-- Índices para tabela `recipes`
 --
 ALTER TABLE `recipes`
   ADD PRIMARY KEY (`id`),
@@ -106,14 +131,14 @@ ALTER TABLE `recipes`
   ADD KEY `category_id` (`category_id`);
 
 --
--- Índices de tabela `users`
+-- Índices para tabela `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- Índices de tabela `votes`
+-- Índices para tabela `votes`
 --
 ALTER TABLE `votes`
   ADD PRIMARY KEY (`id`),
@@ -121,7 +146,7 @@ ALTER TABLE `votes`
   ADD KEY `recipe_id` (`recipe_id`);
 
 --
--- AUTO_INCREMENT para tabelas despejadas
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
@@ -134,33 +159,33 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT de tabela `recipes`
 --
 ALTER TABLE `recipes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `votes`
 --
 ALTER TABLE `votes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- Restrições para tabelas despejadas
+-- Restrições para despejos de tabelas
 --
 
 --
--- Restrições para tabelas `recipes`
+-- Limitadores para a tabela `recipes`
 --
 ALTER TABLE `recipes`
   ADD CONSTRAINT `recipes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `recipes_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
 
 --
--- Restrições para tabelas `votes`
+-- Limitadores para a tabela `votes`
 --
 ALTER TABLE `votes`
   ADD CONSTRAINT `votes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
